917fd8333168a7503abfe94503624f0c0ee63413
Renan Paes <contato_renan@hotmail.com>
2019-06-08 18:00:41 -0300

    Commit da Interface

--- ProjectEulerProblems/Program.cs
+++ ProjectEulerProblems/Program.cs
@@ -6,7 +6,13 @@ namespace ProjectEulerProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("=========================================================================");
+            Console.WriteLine("\n  Olá! Seja bem-vindo à minha maratona de desafios do ProjectEuler.net!\n");
+            Console.WriteLine("=========================================================================");
+            Console.WriteLine("\nDesafios Disponíveis:");
+            Console.WriteLine("\n***AGUARDE, EM BREVE NOVOS DESAFIOS!***");
+            Console.Write("\nDigite o número do desafio: ");
+            Console.ReadKey();
         }
     }
 }
